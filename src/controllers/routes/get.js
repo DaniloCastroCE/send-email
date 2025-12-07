7e1d612dcd20bb17397ef1d1806cda586d796f3a
DaniloCastroCE <38533079+DaniloCastroCE@users.noreply.github.com>
2025-12-07 00:52:53 -0300

    Add files via upload

--- src/controllers/routes/get.js
+++ src/controllers/routes/get.js
@@ -10,11 +10,13 @@ export const help = (req, res) => {
     status: "success",
     message:
       "Bem-vindo à API de envio de e-mails. Abaixo estão as instruções sobre como utilizar a rota '/send-email'.",
+
     usage: {
       endpoint: "/send-email",
       method: "POST",
       description:
         "Esta rota permite enviar um e-mail, podendo incluir um corpo com texto HTML (como rich text ou imagens) e arquivos anexados.",
+
       request_body: {
         from: "Endereço de e-mail do remetente (obrigatório)",
         password:
@@ -22,16 +24,18 @@ export const help = (req, res) => {
         to: "Endereço de e-mail do destinatário (obrigatório)",
         subject: "Assunto do e-mail",
         body: "Corpo do e-mail, podendo ser HTML (obrigatório). Caso queira enviar imagens, cole-as diretamente na área de edição. O conteúdo será convertido automaticamente em base64 e enviado no corpo do e-mail.",
-        host: "Servidor SMTP (opcional, se não fornecido, o valor padrão será 'mail.locktec.com.br')",
+        host: "Servidor SMTP (opcional, se não fornecido o padrão será 'mail.locktec.com.br')",
         files:
           "Arquivos adicionais para anexar ao e-mail (opcional). Envie imagens ou outros tipos de arquivos com o nome 'files'.",
       },
+
       response: {
         status: "success",
         message: "Email enviado com sucesso",
         info: "Informações sobre o envio, como o ID da mensagem.",
-        error: "Mensagem de erro em caso de falha, incluindo detalhes do erro.",
+        error: "Mensagem de erro em caso de falha, incluindo detalhes.",
       },
+
       example_request: {
         from: "exemplo@dominio.com",
         password: "senha_de_app_123",
@@ -46,11 +50,19 @@ export const help = (req, res) => {
           },
         ],
       },
+
       error_handling: {
-        400: "Se algum campo obrigatório não for enviado ou se o corpo do e-mail não for um conteúdo válido, a resposta será '400 Bad Request'.",
-        500: "Se ocorrer um erro interno no servidor, a resposta será '500 Internal Server Error'.",
+        400: "Erro de requisição — campos obrigatórios ausentes ou corpo inválido.",
+        500: "Erro interno do servidor.",
       },
     },
+
+    repository: {
+      link: "https://github.com/DaniloCastroCE/send-email",
+      front_end_exemple:
+        "O arquivo de exemplo está no repositório, na pasta public/html/exemplo.html",
+    },
+
     author: {
       nome: "Danilo de Castro",
       email: "danilocastroce01@gmail.com",
@@ -58,4 +70,6 @@ export const help = (req, res) => {
     },
   });
 };
+
+
 
